2a87c3fcb1b575aa2bae1d79cb8921a7d0f039e1
Nick Bennett <nick@nickbenn.com>
2020-03-10 19:47:56 -0600

    Clean up

--- app/src/main/java/edu/cnm/deepdive/checkapps/ImageAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/checkapps/ImageAdapter.java
@@ -2,60 +2,35 @@ package edu.cnm.deepdive.checkapps;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.graphics.drawable.Drawable;
-import android.util.Log;
+import android.content.pm.PackageManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import java.util.List;
 
 public class ImageAdapter extends ArrayAdapter<ApplicationInfo> {
 
-  public ImageAdapter(@NonNull Context context, int resource) {
-    super(context, resource);
-  }
-
-  public ImageAdapter(@NonNull Context context, int resource, int textViewResourceId) {
-    super(context, resource, textViewResourceId);
-  }
-
-  public ImageAdapter(@NonNull Context context, int resource,
-      @NonNull ApplicationInfo[] objects) {
-    super(context, resource, objects);
-  }
-
-  public ImageAdapter(@NonNull Context context, int resource, int textViewResourceId,
-      @NonNull ApplicationInfo[] objects) {
-    super(context, resource, textViewResourceId, objects);
-  }
+  private final PackageManager manager;
 
   public ImageAdapter(@NonNull Context context, int resource,
       @NonNull List<ApplicationInfo> objects) {
     super(context, resource, objects);
-  }
-
-  public ImageAdapter(@NonNull Context context, int resource, int textViewResourceId,
-      @NonNull List<ApplicationInfo> objects) {
-    super(context, resource, textViewResourceId, objects);
+    manager = context.getPackageManager();
   }
 
   @NonNull
   @Override
   public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-    ImageView view = (ImageView) ((convertView != null) ? convertView
-        : LayoutInflater.from(getContext()).inflate(R.layout.icon_item, parent, false));
-    try {
-      ApplicationInfo info = getItem(position);
-      Drawable icon = getContext().getPackageManager().getApplicationIcon(info.packageName);
-      view.setImageDrawable(icon);
-    } catch (NameNotFoundException e) {
-      Log.e(getClass().getName(), e.getMessage(), e);
-    }
+    View view = (convertView != null) ? convertView
+        : LayoutInflater.from(getContext()).inflate(R.layout.icon_item, parent, false);
+    ApplicationInfo info = getItem(position);
+    ((ImageView) view.findViewById(R.id.icon)).setImageDrawable(info.loadIcon(manager));
+    ((TextView) view.findViewById(R.id.label)).setText(manager.getApplicationLabel(info));
     return view;
   }
 
--- app/src/main/java/edu/cnm/deepdive/checkapps/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/checkapps/MainActivity.java
@@ -3,11 +3,15 @@ package edu.cnm.deepdive.checkapps;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.util.Log;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +21,31 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ListView iconList = findViewById(R.id.icon_list);
-    Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-    mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-    List<ApplicationInfo> packages = getPackageManager().getInstalledApplications(PackageManager.GET_META_DATA);
-    ImageAdapter adapter = new ImageAdapter(this, R.layout.icon_item, packages);
+    List<ApplicationInfo> apps = getApplications();
+    ImageAdapter adapter = new ImageAdapter(this, R.layout.icon_item, apps);
     iconList.setAdapter(adapter);
   }
+
+  private List<ApplicationInfo> getApplications() {
+    PackageManager manager = getPackageManager();
+    Intent intent = new Intent(Intent.ACTION_MAIN);
+    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+    List<ResolveInfo> intents = getPackageManager().queryIntentActivities(intent, 0);
+    List<ApplicationInfo> apps = new ArrayList<>();
+    for (ResolveInfo info : intents) {
+      try {
+        apps.add(manager.getApplicationInfo(info.activityInfo.packageName, 0));
+      } catch (NameNotFoundException e) {
+        Log.e(getClass().getName(), "Can't add app to list", e);
+      }
+    }
+    // HACK This is a terrible way to sort! You should instead create instances of your model class
+    //  (which should implement Comparable), and then sort list of those instances.
+    Collections.sort(apps, (app1, app2) ->
+        manager.getApplicationLabel(app1).toString().compareTo(
+            manager.getApplicationLabel(app2).toString()));
+    return apps;
+  }
+
 }
